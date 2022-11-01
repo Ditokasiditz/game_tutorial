029569eb54f9fd29ccc22c1cdce3f057e1ac19d8
Ditokasiditz <kasidit.samad@gmail.com>
2022-11-02 01:42:02 +0700

    sdlfk

--- tutorial/tutorial/Main.cpp
+++ tutorial/tutorial/Main.cpp
@@ -5,27 +5,34 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "fixed it felix game");
+	sf::RenderWindow window(sf::VideoMode(900, 700), "fixed it felix game");
 
 	sf::Texture pngfelix;
 	pngfelix.loadFromFile("felixnormalleft.png");
 	sf::Sprite felix;
 	felix.setTexture(pngfelix);
-	felix.setScale(1.5, 1.5);
+	felix.setScale(2, 2);
 	felix.setPosition(190,100);
 
 	sf::Texture pngbuilding;
 	pngbuilding.loadFromFile("building.png");
 	sf::Sprite building;
 	building.setTexture(pngbuilding);
-	building.setScale(1.5,1.5);
-	building.setPosition(170,-970);
+	building.setScale(2,2);
+	building.setPosition(151, -1366);
 
 	sf::Texture pnglongbush;
-	pnglongbush.loadFromFile("longbush.png");
+	pnglongbush.loadFromFile("floor.png");
 	sf::Sprite longbush;
 	longbush.setTexture(pnglongbush);
-	longbush.setPosition(0,560);
+	longbush.setPosition(0,640);
+
+	sf::Texture pnggreenwin;
+	pnggreenwin.loadFromFile("greenwindow.png");
+	sf::Sprite greenwin;
+	greenwin.setTexture(pnggreenwin);
+	greenwin.setScale(1.4, 1.4);
+	greenwin.setPosition(207, 24);
 
 	
 	while (window.isOpen())
@@ -35,11 +42,11 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();	
-			if (event.type == sf::Event::MouseMoved)
-			{
-				int xxx = event.mouseMove.x;int yyy = event.mouseMove.y;
-				printf("(%d,%d)",xxx,yyy);
-			}
+			//if (event.type == sf::Event::MouseMoved)
+			//{
+			//	int xxx = event.mouseMove.x;int yyy = event.mouseMove.y;
+			//	printf("(%d,%d)",xxx,yyy);
+			//}
 						
 		}
 		
@@ -56,11 +63,31 @@ int main()
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 			if (position.x < 591)
 				felix.move(0.1, 0.00);
+
+		sf::Vector2f position_green = greenwin.getPosition();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+			greenwin.move(-0.10, 0.00);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+			greenwin.move(0.00, 0.10);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+			greenwin.move(0.00, -0.10);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+			greenwin.move(0.1, 0.00);
+
+		
+		
+		int x = position_green.x;
+		int y = position_green.y;
+		printf("(%d,%d)", x, y);
+		
 		
 		window.clear();
 
-		window.draw(building);
 		window.draw(longbush);
+		window.draw(building);
+		
+		window.draw(greenwin);
+		
 		window.draw(felix);
 
 		window.display();
