3c3e2edbc8fadf14029796628845c1c9dbfe2b3a
Ditokasiditz <kasidit.samad@gmail.com>
2022-11-21 03:28:02 +0700

    add sound

--- tutorial/tutorial/Main.cpp
+++ tutorial/tutorial/Main.cpp
@@ -9,6 +9,7 @@
 #include "brick.h"
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 
 const int maxwin = 15;
@@ -21,7 +22,32 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(900, 700), "fixed it felix game");
 	srand(time(NULL));
 
+	//sound 
+	sf::SoundBuffer game;
+	game.loadFromFile("./sound/01 Running About.flac");
 	
+	sf::SoundBuffer game1;
+	game1.loadFromFile("./sound/08 Miss.flac");
+
+	sf::SoundBuffer game2;
+	game2.loadFromFile("./sound/05 Stage Clear.flac");
+
+	sf::SoundBuffer game3;
+	game3.loadFromFile("./sound/Cartoon.wav");
+
+	sf::Sound themesong;
+	themesong.setBuffer(game);
+	themesong.play();
+
+	sf::Sound coinsound;
+	coinsound.setBuffer(game2);
+
+	sf::Sound diesound;
+	diesound.setBuffer(game1);
+
+	sf::Sound hurtsound;
+	hurtsound.setBuffer(game3);
+	hurtsound.setVolume(30.f);
 
 	//floorstep
 	sf::Texture floorpng;
@@ -36,8 +62,8 @@ int main()
 		floor[i].setScale(0.98, 1);
 		floor_y = floor_y + 155;
 	}
-	
-	
+
+
 	//building
 	building building;
 	building.setposition(151, -1366);
@@ -47,7 +73,7 @@ int main()
 	pnglongbush.loadFromFile("./image/floor.png");
 	sf::Sprite longbush;
 	longbush.setTexture(pnglongbush);
-	longbush.setPosition(0,640);
+	longbush.setPosition(0, 640);
 
 	//green window
 	sf::Texture pnggreenwin;
@@ -82,7 +108,7 @@ int main()
 
 	//brick 
 	std::vector<brick*> brickVec;
-	
+
 	brick brick;
 	brick.setposition(190, 90);
 	brickVec.push_back(&brick);
@@ -107,7 +133,7 @@ int main()
 		coin[i].animationFrame = 0;
 		coinVec.push_back(&coin[i]);
 		coin[i].setposition(x_coin, y_coin);
-		x_coin = x_coin+60;
+		x_coin = x_coin + 60;
 		if (x_coin > 715)
 		{
 			x_coin = 200;
@@ -149,7 +175,7 @@ int main()
 
 
 	//timer
-	double msec100, msec50, msec25, msec125,msec2200;
+	double msec100, msec50, msec25, msec125, msec2200;
 	sf::Clock clock1sec, clock100msec, clock50msec, clock25msec, clock125msec, clock2200msec;
 	sf::Time time1sec, time100msec, time50msec, time25msec, time125msec, time2200msec;
 	clock1sec.restart();
@@ -158,57 +184,63 @@ int main()
 	clock25msec.restart();
 	clock125msec.restart();
 	clock2200msec.restart();
-	
+
 
 	//game event 
 	while (window.isOpen())
 	{
 		felix.reset_movestate();
-		
-		
+
+
 
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
-				window.close();	
+				window.close();
 			if (event.type == sf::Event::MouseMoved)
 			{
-				//int xxx = event.mouseMove.x;int yyy = event.mouseMove.y;
-				//printf("(%d,%d)",xxx,yyy);
-			}			
+				int xxx = event.mouseMove.x;int yyy = event.mouseMove.y;
+				printf("(%d,%d)",xxx,yyy);
+			}
 		}
 
 		brick.falldown();
 		felix.move();
-		//int xxx = felix.getX(); int yyy = felix.getY();
-		//printf("(%d,%d)", xxx, yyy);
-
+		
 
 		//coin logic
 		for (int i = 0; i < coinVec.size(); i++) {
 			if (felix.isCollidingWithCoin(coinVec[i])) {
+				coinsound.play();
 				coinVec[i]->setposition(422234, 423432);
-				score++;
+				score+=10;
 				ssScore.str("");
 				ssScore << "Score " << score;
 				lblScore.setString(ssScore.str());
 			}
 		}
-
+		
 
 		//brick logic
 		for (int i = 0; i < brickVec.size(); i++) {
 			if (felix.isCollidingWithBrick(brickVec[i])) {
 				brickVec[i]->setposition(422234, 423432);
 				lifechance--;
+				if (lifechance == 0)
+				{
+					themesong.pause();
+					diesound.play();
+				}
+				else
+					hurtsound.play();
 				sslife.str("");
 				sslife << "Lifes " << lifechance;
 				lbllife.setString(sslife.str());
 			}
 		}
 
-		
+
 
 
 		window.clear();
@@ -223,14 +255,14 @@ int main()
 		msec2200 = time2200msec.asMilliseconds();
 
 		//reset posx brick
-		if (msec2200 > 2200)
+		if (msec2200 > 3200)
 		{
 			int newx_brick = random_between(150, 724);
-			brick.setposition(newx_brick,90);
+			brick.setposition(newx_brick, 90);
 			clock2200msec.restart();
 		}
 		//coin animation 
-		if(msec125 > 125)
+		if (msec125 > 125)
 		{
 			for (int i = 0; i < maxcoin; i++)
 			{
@@ -240,9 +272,9 @@ int main()
 		}
 
 		//felix animation
-		if (msec100 > 100 )
-		{	
-			felix.animation();		
+		if (msec100 > 100)
+		{
+			felix.animation();
 			clock100msec.restart();
 		}
 
@@ -256,7 +288,7 @@ int main()
 		for (int i = 0; i < 3; i++)
 			window.draw(floor[i]);
 
-		for(int i=0;i<5;i++)
+		for (int i = 0; i < 5; i++)
 			window.draw(greenwin[i]);
 
 		for (int i = 0; i < maxwin; i++)
@@ -272,8 +304,8 @@ int main()
 		felix.draw(window); ///draw sprite
 
 		brick.draw(window);
-		
-		
+
+
 
 		window.display();
 	}
